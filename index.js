fc3eaf662c6d6171a611e2223d47138ff2191f85
Ragini <raginiv743@gmail.com>
2023-06-10 00:03:32 +0530

    Added initial projects file

---
+++ index.js
@@ -0,0 +1,20 @@
+input=document.querySelector("input");
+buttons=document.querySelectorAll(".button");
+for(let button of buttons){
+    button.addEventListener("click",(e)=>{
+        if(e.target.textContent=="="){
+            console.log("hello")
+            input.value=eval(input.value)
+            console.log(input.value)
+
+        }
+        else if(e.target.textContent=="clear"){
+            input.value=""
+        }
+        else{
+        console.log(e.target)
+        input.value+=e.target.textContent}
+
+    }
+ )
+}
